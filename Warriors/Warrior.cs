2d570f20cbda35213b3c3b2a07d3ff0233655576
Jason Edgar <jason.edgar@gmail.com>
2020-07-25 23:03:20 -0400

    Add project files.

---
+++ Warriors/Warrior.cs
@@ -0,0 +1,51 @@
+﻿using WarriorWars.Enum;
+using WarriorWars.Equipment;
+
+namespace WarriorWars
+{
+    class Warrior
+    {
+        private const int GOOD_GUY_STARTING_HEALTH = 100;
+        private const int BAD_GUY_STARTING_HEALTH = 100;
+        
+        private readonly Faction FACTION;
+
+        private int health;
+        private string name;
+
+        private bool isAlive;
+        public bool IsAlive
+        {
+            get
+            {
+                return isAlive;
+            }
+        }
+
+        private Weapon weapon;
+        private Armor armor;
+
+        public Warrior(string name, Faction faction)
+        {
+            this.name = name;
+            FACTION = faction;
+            isAlive = true;
+
+            switch (faction)
+            {
+                case Faction.GoodGuy:
+                    weapon = new Weapon(faction);
+                    armor = new Armor(faction);
+                    health = GOOD_GUY_STARTING_HEALTH;
+                    break;
+                case Faction.BadGuy:
+                    weapon = new Weapon(faction);
+                    armor = new Armor(faction);
+                    health = BAD_GUY_STARTING_HEALTH;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
